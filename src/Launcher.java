f73061a24cb6514388c1c369ee8983d10bccd362
Theo__NOMMAY <52563906+Laran38@users.noreply.github.com>
2021-04-09 21:37:49 +0200

    Add files via upload

---
+++ src/Launcher.java
@@ -0,0 +1,10 @@
+import fr.uga.pddl4j.tutorial.asp.ParserPlanner;
+
+public class Launcher {
+	/*
+	 * On commence par parser les arguments
+	 */
+	public static void main(String[] args) {
+	 	ParserPlanner.start(args);
+	}
+}
