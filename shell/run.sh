5386b27ef13e5fbed119b6b3805eb53bbba96a94
Nicu Vrabii <nicuvrabii1@gmail.com>
2021-04-28 19:49:55 +0200

    updates shell scripts

--- shell/run.sh
+++ shell/run.sh
@@ -1,4 +1,4 @@
-CP="src/lib/org.sat4j-csp.jar:src/lib/org.sat4j.pb-src.jar:src/lib/org.sat4j.pb.jar:src/lib/pddl4j-3.8.3.jar:src/lib/sat4j-csp.jar:build"
+CP="lib/org.sat4j-csp.jar:lib/org.sat4j.pb-src.jar:lib/org.sat4j.pb.jar:lib/pddl4j-3.8.3.jar:lib/sat4j-csp.jar:build"
 
 if [ -z "$1$2$3" ]
 then
