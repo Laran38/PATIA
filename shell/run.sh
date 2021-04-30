5361f6d48768826490a862b6c420f43fffbd661a
Laran38 <yulo69@yahoo.fr>
2021-04-30 10:18:54 +0200

    type de fichier shell change

--- shell/run.sh
+++ shell/run.sh
@@ -4,5 +4,5 @@ if [ -z "$1$2$3" ]
 then
     echo "Usage: shell/run.sh <-a, -s, -p> <domain>.pddl <problem>.pddl\n"
 else
-    java -server -Xms16384m -Xmx16384m -cp $CP Launcher $1 -o $2 -f $3 
+    java -server -Xms16384m -Xmx16384m -cp $CP Launcher $1 -o $2 -f $3
 fi
